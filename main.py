26433949c87aac584f331dab213e6426eae04f77
Ryan Moon <ryanmoon2050@gmail.com>
2023-03-21 17:31:50 -0400

    implemented entries, fixed image display

--- main.py
+++ main.py
@@ -1,76 +1,61 @@
 from tkinter import *
 from tkinter import messagebox
+from PIL import Image, ImageTk
 import json
 import random
 
 # TODO fix the displaying of images
 # TODO get rid of buttons, make text entries
 
-FONT1 = ("New Courier", 18, "normal")
-
+FONT1 = ("Times New Roman", 24, "normal")
+S_FONT = ("Times New Roman", 18, "normal")
 
 class SpinTheAtom:
     def __init__(self):
         self.curr_ans = []
-        self.keys = {
-            "category": "",
-            "difficulty": ""
-        }
+
         self.categories = ["Atomic Structure", "Periodic Table and Electrons", "Bonding and Naming",
                            "Types of Reactions \nand Balancing Equations", "Stoichiometry", "Energy"]
         self.difficulties = ["E", "M", "H"]
 
         self.window = Tk()
         self.window.title("Spin The Atom!")
-        self.window.geometry("800x600")
+        self.window.geometry("1200x600")
         self.window.resizable(True, True)
         self.window.config(padx=50, pady=50)
 
-        # category chooser
-        self.category_label = Label(text="Click Below!", font=FONT1)
+        # category entry
+        self.category_label = Label(text="Enter the Category Number Below!", font=FONT1)
         self.category_label.grid(row=0, column=0)
 
-        self.category_button = Button(text="Get Category", width=20, command=self.get_category)
-        self.category_button.config(pady=15)
-        self.category_button.grid(row=1, column=0)
+        self.category_entry = Entry(width=20, font=FONT1)
+        self.category_entry.grid(row=1, column=0)
 
-        # difficulty chooser
-        self.difficulty_label = Label(text="Click Below!", font=FONT1)
+        # difficulty entry
+        self.difficulty_label = Label(text="Enter the Difficulty Below \n(first letter, uppercased)", font=FONT1)
         self.difficulty_label.grid(row=0, column=2)
 
-        self.difficulty_button = Button(text="Get Question!", width=20, command=self.get_difficulty)
-        self.difficulty_button.grid(row=1, column=2)
-        self.difficulty_button.config(pady=15)
+        self.difficulty_entry = Entry(width=20, font=FONT1)
+        self.difficulty_entry.grid(row=1, column=2)
 
-        self.get_question_button = Button(text="Get Question!", width=20, command=self.get_question)
+        self.get_question_button = Button(text="Get Question!", width=15, command=self.get_question, font=S_FONT)
         self.get_question_button.grid(row=2, column=1)
 
         self.question_label = Label(text="", font=FONT1)
-        self.question_label.grid(row=3, column=1)
+        self.question_label.grid(row=3, column=0, columnspan=3)
         self.question_label.config(pady=20)
 
-        self.get_a_button = Button(text="Get Answer!", width=20, command=self.get_ans)
+        self.get_a_button = Button(text="Get Answer!", width=15, command=self.get_ans, font=S_FONT)
         self.get_a_button.grid(row=4, column=1)
 
         self.ans_label = Label(text="", font=FONT1)
-        self.ans_label.grid(row=5, column=1)
-        self.ans_label.config(pady=20)
+        self.ans_label.grid(row=5, column=0, columnspan=3)
 
-        self.canvas = Canvas(self.window, width=800, height=500)
-        self.curr_image = PhotoImage(file="images/C1a.png")
-        self.displayed_image = self.canvas.create_image(250, 250, image=self.curr_image)
-        self.canvas.grid(row=6, column=0, columnspan=3)
+        self.tk_image = PhotoImage(file="images/blank_for_SpinTheAtom.png")
+        self.image_display = Label(width=800, height=300, image=self.tk_image)
+        self.image_display.grid(row=6, column=0, columnspan=3, sticky="NE")
 
         self.window.mainloop()
-
-    def get_difficulty(self):
-        difficulty = random.choice(self.difficulties)
-
-        # update label
-        self.difficulty_label.config(text=difficulty)
-
-        # add to list
-        self.keys["difficulty"] = difficulty
 
     def get_question(self):
         self.curr_ans.clear()
@@ -79,11 +64,15 @@ class SpinTheAtom:
         with open("questions.json") as file:
             data = json.load(file)  # save the data in a var
 
-        c = self.keys["category"]
-        d = self.keys["difficulty"]
+        d = self.difficulty_entry.get()
+        c = self.category_entry.get()
+        
+        # check if valid inputs
+        if d not in self.difficulties:
+            messagebox.showerror(title="Input Error", message='Please enter in the first letter of the difficulty, capitalized.\nEx: for medium, "M"')
 
-        if c == "" or d == "":
-            messagebox.showerror(message="Make sure to get Category and Question first!")
+        if int(c) not in range(1, 7):
+            messagebox.showerror(title="Input Error", message="Please enter in a number between 1 and 6")
 
         possible_questions = list(data[c][d].items())
 
@@ -91,16 +80,19 @@ class SpinTheAtom:
         print(question_obj)
         question = question_obj["question"]
         ans = question_obj["answer"]
-        image = question_obj["image"]
+        image_path = question_obj["image"]
 
         # update question label
         self.question_label.config(text=question)
         self.curr_ans.append(ans)
 
         # update canvas
-        if image != "images/NO IMAGE.png":
-            self.curr_image = PhotoImage(file=image)
-            self.canvas.itemconfig(self.displayed_image, image=self.curr_image)
+        if image_path == "images/NO IMAGE.png":
+            self.tk_image = PhotoImage(file="images/blank_for_SpinTheAtom.png")
+            self.image_display.config(image=self.tk_image)
+        else:
+            self.tk_image = PhotoImage(file=image_path)
+            self.image_display.config(image=self.tk_image)
         # try:
         #     with open("questions.json") as file:
         #         data = json.load(file)  # read the data
@@ -129,19 +121,6 @@ class SpinTheAtom:
 
     def get_ans(self):
         self.ans_label.config(text=f"{self.curr_ans[0]}")
-
-    def get_category(self):
-        num = random.randint(1, 6)
-        # random.randint is inclusive
-
-        # find the actual category
-        category = self.categories[num - 1]
-
-        # update label
-        self.category_label.config(text=category)
-
-        # add number to list
-        self.keys["category"] = str(num)
 
 
 if __name__ == "__main__":
